ab7d1952f525f1036c06ed4228fe49b34a684910
Shahadat900 <mrshahadat075@gmail.com>
2026-03-16 23:00:57 +0600

    Add initial index.php for Shahadat Host Dashboard

---
+++ www/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Shahadat Host</title>
+<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<h1>🚀 Shahadat Host Dashboard</h1>
+
+<form method="POST" action="tables.php">
+
+Host
+<input name="host" value="localhost">
+
+User
+<input name="user" value="root">
+
+Password
+<input name="pass">
+
+Database
+<input name="db">
+
+<button>Connect</button>
+
+</form>
+
+<br>
+
+<a href="sql.php">SQL Runner</a>
+
+</body>
+</html>
